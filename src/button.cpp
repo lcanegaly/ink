4e201b1b3b65bfbadfe255af1d41ac433a9e7ff4
lee <lcanegaly@gmail.com>
2023-06-04 18:42:14 +0000

    Broke out the logic for checking if button is clicked into a private method. Cleaned up formatting in Button.

--- src/button.cpp
+++ src/button.cpp
@@ -6,22 +6,22 @@
 
 //TODO.. Updatedelegate should actually do something. 
 Button::Button(const char* filepath):
-  Object(new ButtonUpdate, new DrawTexture(this)), 
-  input_delegate_{new GLFWInput()}, callback_{nullptr} {
+    Object(new ButtonUpdate, new DrawTexture(this)), 
+    input_delegate_{new GLFWInput()}, callback_{nullptr} {
   Load(filepath);
 }
 
 Button::Button(const char* filepath, int width, int height, int X, int Y): 
-  Object(new ButtonUpdate, new DrawTexture(this), new Silent()), input_delegate_{new GLFWInput()}, 
-  callback_{nullptr} {
+    Object(new ButtonUpdate, new DrawTexture(this), new Silent()), 
+    input_delegate_{new GLFWInput()}, callback_{nullptr} {
   set_size(glm::vec2(width, height));
   set_position(glm::vec2(X,Y), 0); //TODO - fix, should take rotation from constructor.
   Load(filepath);
 }
 
-Button::Button(const char* filepath, glm::vec2 size, glm::vec2 position, Callback_T* callback) : 
-  Object(new ButtonUpdate, new DrawTexture(this)), input_delegate_{new EmscriptenInput()}, 
-  callback_{callback} {
+Button::Button(const char* filepath, glm::vec2 size, glm::vec2 position,
+    Callback_T* callback): Object(new ButtonUpdate, new DrawTexture(this)), 
+    input_delegate_{new EmscriptenInput()}, callback_{callback} {
   set_size(size);
   set_position(position, 0); //TODO - fix, should take rotation from constructor.
   Load(filepath);
@@ -35,30 +35,30 @@ bool Button::active() {
   return false;
 }
 
-// TODO - Fix this nexted if statement mess.. 
+bool Button::IsClicked() {
+  if (input_delegate_->GetMouseClick()) {
+    int x = std::abs(input_delegate_->GetMousePosition().x - position().x);
+		int y = std::abs(input_delegate_->GetMousePosition().y - position().y);
+    elapsed_time_ = 0;
+		if ( (x <= 0.5 * size().x) && ( y <= 0.5 * size().y )) {
+      clicked_ = true;
+      return true; 
+    }
+  }
+  return false;
+}
+
 void Button::Update(std::time_t delta_t) {
   elapsed_time_ += delta_t; 
-  if (elapsed_time_ > 500){
-    if (input_delegate_){
-      if (input_delegate_->GetKey(65)) {
-      }
+  if (elapsed_time_ > 300){
+    if (IsClicked()){
+      if (execute != nullptr){
+        execute();
+      } 
+      if (callback_) {
+        callback_->Call();
+      } 
     }
-    if (input_delegate_->GetMouseClick()) {
-		  int x = std::abs(input_delegate_->GetMousePosition().x - this->position().x);
-		  int y = std::abs(input_delegate_->GetMousePosition().y - this->position().y);
-
-      elapsed_time_ = 0;
-		  if ( (x <= 0.5 * this->size().x) && ( y <= 0.5 * this->size().y )) {
-        clicked_ = true;
-        if (execute != nullptr){
-          execute();
-        } 
-        input_delegate_->Reset();
-        if (callback_) {
-          callback_->Call();
-        } 
-      }
-  	}
   }
 }
 
--- src/button.h
+++ src/button.h
@@ -1,21 +1,26 @@
 #pragma once
+#include <functional>
+#include <chrono>
+
 #include "object.h"
 #include "image.h"
 #include "callback.h"
 #include "vec2.h"
-#include <functional>
-#include <chrono>
 
-class Button : public Object {//, public ImageInterface {
+class Button : public Object {
  public:
   Button(const char* filepath); 
   Button(const char* filepath, int width, int height, int x, int y); 
-  Button(const char* filepath, glm::vec2 size, glm::vec2 position, Callback_T* callback); 
-  //void Draw(int width, int height, int x, int y) override; 
+  Button(const char* filepath, glm::vec2 size, glm::vec2 position, 
+      Callback_T* callback); 
   void Update(std::time_t delta_t) override;
   void RegisterCallback(Callback_T* callback);
   void SetExecute(std::function<void()> func);
   bool active(); 
+ 
+ private:
+  bool IsClicked();
+
  private:
   std::unique_ptr<InputDelegate> input_delegate_;
   Callback_T* callback_;
